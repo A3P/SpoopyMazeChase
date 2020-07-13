c330f8bf07fc6dd188ead5f5690357ffafdb9f56
Ryan Afshar <ryanafsharryan@gmail.com>
2020-07-13 16:10:55 -0700

    Add keyword recognition for chapter vocabObjs

--- Assets/Scripts/VocabListener.cs
+++ Assets/Scripts/VocabListener.cs
@@ -8,17 +8,28 @@ public class VocabListener : MonoBehaviour
     public ChapterObjects chapter;
     public ConfidenceLevel confidence = ConfidenceLevel.Medium;
 
+    protected PhraseRecognizer recognizer;
+
     private string[] keywords;
     // Start is called before the first frame update
     void Start()
     {
         SetVocabNames();
+        recognizer = new KeywordRecognizer(keywords, confidence);
+        recognizer.OnPhraseRecognized += OnVocabRecognized;
+        recognizer.Start();
+        Debug.Log("Recognizer running: " + recognizer.IsRunning);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnVocabRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Recognized: " + args.text);
     }
 
     private void SetVocabNames()
