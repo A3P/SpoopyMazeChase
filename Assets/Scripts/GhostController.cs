baa78426f040a5811a026298ebe70a8ccf80c9bf
Ryan Afshar <ryanafsharryan@gmail.com>
2020-07-15 13:30:00 -0700

    Add chasing ghost

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class GhostController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float minDistance;
+
+    private FirstPersonController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FirstPersonController.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(FirstPersonController.Instance.transform);
+        // rotation to make up for the opposite forward vector of the model
+        transform.localRotation *= Quaternion.Euler(0, 180, 0);
+        if (Vector3.Distance(transform.position, player.transform.position) > minDistance)
+        {
+            // Subtraction instead of addition due to model
+            transform.position -= transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+}
