56062bf0ff41cb0b52bf410a534843d175846f26
Ryan Afshar <ryanafsharryan@gmail.com>
2020-07-20 11:53:12 -0700

    Add timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text timerText;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerText.text = (Time.time - startTime).ToString("0.0") + "s";
+    }
+}
