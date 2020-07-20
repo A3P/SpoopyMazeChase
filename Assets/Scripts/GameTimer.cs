6d8f0c5d10d2d6893d96ee67a32542cf83c17d3b
Ryan Afshar <ryanafsharryan@gmail.com>
2020-07-20 12:18:58 -0700

    Rename timer, change it to a singleton, and add EndTimer function

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public static GameTimer Instance;
+
+    [SerializeField]
+    private TMP_Text timerText;
+    private float startTime;
+    private Boolean paused = false;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!paused)
+            timerText.text = (Time.time - startTime).ToString("0.0") + "s";
+    }
+
+    public void EndTimer()
+    {
+        paused = true;
+    }
+}
