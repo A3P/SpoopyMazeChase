8de12c1fd7d226b831302c957d23b1a1c401c7c9
Ryan Afshar <ryanafsharryan@gmail.com>
2020-07-21 16:23:22 -0700

    Battery icon will change colors to red when empty

--- Assets/Scripts/UI/BatteryUI.cs
+++ Assets/Scripts/UI/BatteryUI.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class BatteryUI : MonoBehaviour
 {
     public RectTransform batteryLevel;
+    public Color regularColor;
+    public Color emptyColor;
 
     private Vector2 originalSize;
     
@@ -24,5 +26,14 @@ public class BatteryUI : MonoBehaviour
     public void SetChargeLevel(float chargePercent)
     {
         batteryLevel.sizeDelta = new Vector2(chargePercent * originalSize.x, originalSize.y);
+        if(chargePercent <= 0f)
+        {
+            SetColor(emptyColor);
+        }
+    }
+
+    public void SetColor(Color color)
+    {
+        GetComponent<RawImage>().color = color;
     }
 }
