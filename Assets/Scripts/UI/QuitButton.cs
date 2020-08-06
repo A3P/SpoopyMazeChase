a66d3083eec125c77e7c3c73b3f8f027dd311c11
Glen McManus <infusionedutainmentgm@gmail.com>
2020-08-06 15:19:05 -0800

    add pause menu

---
+++ Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+#if UNITY_EDITOR
+        Debug.Log("OnClick quit");
+#endif
+
+        Application.Quit();
+    }
+}
